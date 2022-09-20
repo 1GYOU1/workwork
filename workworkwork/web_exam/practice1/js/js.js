09718a824a6daa9e94e9642af382fe25a1d942b2
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2022-09-20 11:26:24 +0900

    2022-09-20

--- workworkwork/web_exam/practice1/js/js.js
+++ workworkwork/web_exam/practice1/js/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	// menu
+// menu
 $('.menu').hover(function(){
 	$('.sub_menu').stop().slideDown();
 })
@@ -28,14 +28,27 @@ $('button').click(function(){
 	$('.pop').fadeOut();
 })
 
-//fade slide
-$('.slider img').hide();
-$('.slider img:first-child').show();
+//상하슬라이딩
 setInterval(function(){
-    $('.slider img:first-child').fadeOut()
-    .next('img').fadeIn()
-    .end().appendTo('.slider');
-}, 2500);
+	$('.slider').delay(2500);
+	$('.slider').animate({marginTop:'-300px'});
+	$('.slider').delay(2500);
+	$('.slider').animate({marginTop:'-600px'});
+	$('.slider').delay(2500);
+	$('.slider').animate({marginTop:'0px'});
+});
+
+});
+
+//fade slide
+// $('.slider img').hide();
+// $('.slider img:first-child').show();
+
+// setInterval(function(){
+//     $('.slider img:first-child').fadeOut()
+//     .next('img').fadeIn()
+//     .end().appendTo('.slider');
+// }, 2500);
 
 // let imgs = 2;
 // let now = 1;
@@ -45,27 +58,27 @@ setInterval(function(){
 //     now = now == imgs ? 0 : now += 1;
 // }, 3000);
 
-});
+// let imgNum = 2;
+// let now = 1;
+// console.log($(".slider img").eq())
+// setInterval(function(){
+// 	$(".slider img").eq((now - 1)).fadeOut();
+// 	$(".slider img").eq(now).fadeIn();
+// 	if(now === imgNum){
+// 		now = 0;
+// 	}else{
+// 		now = now + 1;
+// 	}
+// },2000)
 
 // //좌우슬라이딩
 // setInterval(function(){
-//     $('#slide1>ul').delay(2500);
-//     $('#slide1>ul').animate({marginLeft: "-1200px"})
-//     $('#slide1>ul').delay(2500);
-//     $('#slide1>ul').animate({marginLeft: "-2400px"})
-//     $('#slide1>ul').delay(2500);
-//     $('#slide1>ul').animate({marginLeft: "0px"})
-// });
-
-
-// //상하슬라이딩
-// setInterval(function(){
-//     $('#slide2>ul').delay(2500);
-//     $('#slide2>ul').animate({marginTop: "-300px"})
-//     $('#slide2>ul').delay(2500);
-//     $('#slide2>ul').animate({marginTop: "-600px"})
-//     $('#slide2>ul').delay(2500);
-//     $('#slide2>ul').animate({marginTop: "0px"})
+//     $('.slider').delay(2500);
+//     $('.slider').animate({marginLeft: "-1200px"})
+//     $('.slider').delay(2500);
+//     $('.slider').animate({marginLeft: "-2400px"})
+//     $('.slider').delay(2500);
+//     $('.slider').animate({marginLeft: "0px"})
 // });
 
 // https://sudal89.tistory.com/47
