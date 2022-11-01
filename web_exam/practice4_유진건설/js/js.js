da10cb28c4dc66a6dff1a2e0877a15cb4d7e2097
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2022-11-01 20:56:19 +0900

    2022-11-01

--- web_exam/practice4_유진건설/js/js.js
+++ web_exam/practice4_유진건설/js/js.js
@@ -1,1 +1,31 @@
-//slider
+//menu
+$('.main_menu > ul > li > a').on('mouseenter',function(){
+	$('.sub_menu').stop().slideDown();
+	$('.menu_bg').stop().slideDown();
+});
+$('.main_menu').on('mouseleave',function(){
+	$('.sub_menu').stop().slideUp();
+	$('.menu_bg').stop().slideUp();
+});
+
+//slider fade
+var num=0;
+var img=2;
+setInterval(slideEvent, 2000);
+function slideEvent(){
+	$('.slider li').eq(num).fadeIn();
+	$('.slider li').eq(num-1).fadeOut();
+	if(num == img){
+		num = 0;
+	}else{
+		num++;
+	}
+}
+
+//popup
+$('.notice_cont > ul > li:first-child').on('click',function(){
+	$('.popup').fadeIn();
+});
+$('.popup > button').on('click',function(){
+	$('.popup').fadeOut();
+});
